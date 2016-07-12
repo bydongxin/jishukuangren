49870681df8d0e16e65898dd664558a9fc187150
WangDingXu <wdxchn@163.com>
2016-07-12 23:39:39 +0800

    增加Tab页

---
+++ JSKR.NET.Web/JSKR.Web/Controllers/LuceneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSKR.Web.Controllers
+{
+    public class LuceneController : Controller
+    {
+        // GET: Lucene
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ JSKR.NET.Web/JSKR.Web/Scripts/CommonScript.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    bindStyle();
+
+    /**
+     * 根据控制器名称设置nav样式
+     */
+    function bindStyle() {
+        var $li = $('.nav.navbar-nav>li>a[href="/' + $('[name=hidCName]').val() + '"]').parent();
+        $li.siblings().removeClass('active');
+        $li.addClass('active');
+    }
+});
