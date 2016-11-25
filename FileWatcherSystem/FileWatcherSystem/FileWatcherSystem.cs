ab7a28098b671ba9c45603b7e6f74a6bffc53b39
wdx <wdxchn@163.com>
2016-11-25 17:38:04 +0800

    自己写的一个项目发布的Winfrom程序

---
+++ FileWatcherSystem/FileWatcherSystem/FileWatcherSystem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WatcherHelper;
+
+namespace FileWatcherSystem
+{
+    public partial class FileWatcherSystem : Form
+    {
+        public FileWatcherSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartWatcher_Click(object sender, EventArgs e)
+        {
+            MyFileSystemWather myWather = new MyFileSystemWather(@"D:\test", "*.txt");
+            myWather.OnChanged += OnChanged;
+            myWather.OnCreated += OnCreated;
+            myWather.OnRenamed += OnRenamed;
+            myWather.OnDeleted += OnDeleted;
+            myWather.Start();
+
+        }
+
+        private static void OnCreated(object source, FileSystemEventArgs e)
+        {
+
+            Console.WriteLine("文件新建事件处理逻辑");
+
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            FileInfo changeFile = new FileInfo(e.FullPath);
+            changeFile.CopyTo(@"D:\test1\" + e.Name, true);
+            Console.WriteLine("文件改变事件处理逻辑");
+        }
+
+        private static void OnDeleted(object source, FileSystemEventArgs e)
+        {
+
+            Console.WriteLine("文件删除事件处理逻辑");
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+
+            Console.WriteLine("文件重命名事件处理逻辑");
+        }
+
+
+    }
+}
---
+++ FileWatcherSystem/WatcherHelper/MyFileSystemWather.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WatcherHelper
+{
+    public delegate void Completed(string key);
+
+    public class MyFileSystemWather
+    {
+        private readonly FileSystemWatcher fsWather;
+
+        private readonly Hashtable hstbWather;
+
+        public event RenamedEventHandler OnRenamed;
+        public event FileSystemEventHandler OnChanged;
+        public event FileSystemEventHandler OnCreated;
+        public event FileSystemEventHandler OnDeleted;
+
+        /// <summary> 
+        /// 构造函数 
+        /// </summary> 
+        /// <param name="path">要监控的路径</param>
+        /// <param name="filter"></param> 
+        public MyFileSystemWather(string path, string filter)
+        {
+            if (!Directory.Exists(path))
+            {
+                throw new Exception("哟，小伙，这路径找不到呀：" + path);
+            }
+
+            hstbWather = new Hashtable();
+
+            fsWather = new FileSystemWatcher(path)
+            {
+                IncludeSubdirectories = true,
+                Filter = filter
+            };
+            // 是否监控子目录
+            fsWather.Renamed += fsWather_Renamed;
+            fsWather.Changed += fsWather_Changed;
+            fsWather.Created += fsWather_Created;
+            fsWather.Deleted += fsWather_Deleted;
+        }
+
+        /// <summary> 
+        /// 开始监控 
+        /// </summary> 
+        public void Start()
+        {
+            fsWather.EnableRaisingEvents = true;
+        }
+
+        /// <summary> 
+        /// 停止监控 
+        /// </summary> 
+        public void Stop()
+        {
+            fsWather.EnableRaisingEvents = false;
+        }
+
+        /// <summary> 
+        /// filesystemWatcher 本身的事件通知处理过程 
+        /// </summary> 
+        /// <param name="sender"></param> 
+        /// <param name="e"></param> 
+        private void fsWather_Renamed(object sender, RenamedEventArgs e)
+        {
+            lock (hstbWather)
+            {
+                hstbWather.Add(e.FullPath, e);
+            }
+
+            WatcherProcess watcherProcess = new WatcherProcess(sender, e);
+            watcherProcess.OnCompleted += new Completed(WatcherProcess_OnCompleted);
+            watcherProcess.OnRenamed += new RenamedEventHandler(WatcherProcess_OnRenamed);
+            Thread thread = new Thread(watcherProcess.Process);
+            thread.Start();
+        }
+
+        private void WatcherProcess_OnRenamed(object sender, RenamedEventArgs e)
+        {
+            OnRenamed?.Invoke(sender, e);
+        }
+
+        private void fsWather_Created(object sender, FileSystemEventArgs e)
+        {
+            lock (hstbWather)
+            {
+                hstbWather.Add(e.FullPath, e);
+            }
+            WatcherProcess watcherProcess = new WatcherProcess(sender, e);
+            watcherProcess.OnCompleted += new Completed(WatcherProcess_OnCompleted);
+            watcherProcess.OnCreated += new FileSystemEventHandler(WatcherProcess_OnCreated);
+            Thread threadDeal = new Thread(watcherProcess.Process);
+            threadDeal.Start();
+        }
+
+        private void WatcherProcess_OnCreated(object sender, FileSystemEventArgs e)
+        {
+            OnCreated?.Invoke(sender, e);
+        }
+
+        private void fsWather_Deleted(object sender, FileSystemEventArgs e)
+        {
+            lock (hstbWather)
+            {
+                hstbWather.Add(e.FullPath, e);
+            }
+            WatcherProcess watcherProcess = new WatcherProcess(sender, e);
+            watcherProcess.OnCompleted += new Completed(WatcherProcess_OnCompleted);
+            watcherProcess.OnDeleted += new FileSystemEventHandler(WatcherProcess_OnDeleted);
+            Thread tdDeal = new Thread(watcherProcess.Process);
+            tdDeal.Start();
+        }
+
+        private void WatcherProcess_OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            OnDeleted?.Invoke(sender, e);
+        }
+
+        private void fsWather_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                if (hstbWather.ContainsKey(e.FullPath))
+                {
+                    WatcherChangeTypes oldType = ((FileSystemEventArgs)hstbWather[e.FullPath]).ChangeType;
+                    if (oldType == WatcherChangeTypes.Created || oldType == WatcherChangeTypes.Changed)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            lock (hstbWather)
+            {
+                hstbWather.Add(e.FullPath, e);
+            }
+            WatcherProcess watcherProcess = new WatcherProcess(sender, e);
+            watcherProcess.OnCompleted += new Completed(WatcherProcess_OnCompleted);
+            watcherProcess.OnChanged += new FileSystemEventHandler(WatcherProcess_OnChanged);
+            Thread thread = new Thread(watcherProcess.Process);
+            thread.Start();
+        }
+
+        private void WatcherProcess_OnChanged(object sender, FileSystemEventArgs e)
+        {
+            OnChanged?.Invoke(sender, e);
+        }
+
+        private void WatcherProcess_OnCompleted(string key)
+        {
+            lock (hstbWather)
+            {
+                hstbWather.Remove(key);
+            }
+        }
+    }
+}
