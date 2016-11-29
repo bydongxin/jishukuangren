40a12c9b9862d236c3b4bdee05943b8a6028e26a
wdx <wdxchn@163.com>
2016-11-29 22:46:23 +0800

    修改Web，不是用阿里云CDN

--- JSKR.NET.Web/JSKR.Web/Controllers/CSharpController.cs
+++ JSKR.NET.Web/JSKR.Web/Controllers/CSharpController.cs
@@ -1,17 +1,49 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace JSKR.Web.Controllers
 {
     public class CSharpController : Controller
     {
         // GET: CSharp
-        public ActionResult Index()
+        public ActionResult Index(string[] key, string[] value)
         {
+            Model model = new Model();
+            
+            List<KeyValueList> list = new List<KeyValueList>();
+            if (key != null)
+            {
+                for (int i = 0; i < key.Length; i++)
+                {
+                    list.Add(new KeyValueList(key[i], value[i]));
+                }
+            }
+            model.List = list;
+            string str = new JavaScriptSerializer().Serialize(model);
             return View();
         }
     }
+
+    public class Model
+    {
+        public object List { get; set; }
+    }
+
+    public class KeyValueList
+    {
+        public KeyValueList(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
 }
